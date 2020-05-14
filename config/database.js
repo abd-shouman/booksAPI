6e79aea4a6dedb195c78660157c39c38b9adaa77
abd-shouman <amsh-1992@hotmail.com>
2020-05-15 00:13:56 +0100

    added error logging for DB connection

--- config/database.js
+++ config/database.js
@@ -2,13 +2,36 @@ import mongoose, { Connection } from "mongoose"
 
 class DBConnection{
     constructor(){
-        const url = `mongodb+srv://admin:${process.env.DBPASS}@cluster0-zuw0b.mongodb.net/test?retryWrites=true&w=majority`;
+        console.log("DBConnection Constructor")
+        const url = `mongodb+srv://admin:${process.env.DBPASS}@cluster0-l03qk.mongodb.net/bookapi?retryWrites=true&w=majority` 
+
         mongoose.Promise = global.Promise;
         mongoose.set('useNewUrlParser', true);
         mongoose.set('useFindAndModify', false);
         mongoose.set('useCreateIndex', true);
         mongoose.set('useUnifiedTopology', true);
-        mongoose.connect(url);
+        
+        // mongoose.connect(url).
+        // catch(error => {
+        //     console.log("On initial connection error");
+        //     console.log(error)
+        //     // handleError(error)
+        // });
+
+        console.log("trying to establish connection")
+        mongoose.connect(url).then(
+            () => { console.log('Successful connection') },
+            (err) => { 
+                console.log('Could not establish connection');
+                console.log(err);
+            }
+        );
+        
+        mongoose.connection.on('error', err => {
+            console.log(`loggin onConnection error`)
+            console.log(err)
+            mongoose.close();
+        });
     }
 }
 
