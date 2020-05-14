6fb306f1a0ff13d4ab985a7bfcf78ade7fb1337d
abd-shouman <amsh-1992@hotmail.com>
2020-05-15 00:11:45 +0100

    updated book router to add post method ... added functionalty to return meaninful messages

--- books/index.js
+++ books/index.js
@@ -1,12 +1,31 @@
 const express = require("express"),
-    app = express()
+    app = express();
+
+import booksModel from "../src/models/book.model";
 
 app.get("/", (req,res)=>{
-    return res.status(200).json("you have to provide an ID for the book you need");
+    return res.status(200).json(`${booksModel.getAll()}`);
 })
 
 app.get("/:id", (req,res)=>{
     return res.status(200).json(`You have requested Book ${req.params.id}`)
+})
+
+app.post("/add", (req,res)=>{
+    console.log(req.body);
+    booksModel.addBook(req.body)
+    .then(addedBook=>{
+        console.log("book saved successfully");
+        console.log(addedBook);
+        return res.json(addedBook);
+    })
+    .catch( (err)=>{
+        console.log('could not add book');
+        console.log(err);
+        return res.json(err);
+    })
+    
+    
 })
 
 module.exports  = app
